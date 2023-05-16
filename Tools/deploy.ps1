581765f97a92d1fe05b0a71eb61be57027f44d22
Bruno <b.b.da.costa@gmail.com>
2023-05-16 15:23:02 -0300

    🔧 Push tags created when deploying to repository

--- Tools/deploy.ps1
+++ Tools/deploy.ps1
@@ -30,7 +30,7 @@ $PACKAGE_JSON = Get-Content $PACKAGE_FILE | ConvertFrom-Json
 $PACKAGE_JSON.version = $VERSION
 ConvertTo-Json $PACKAGE_JSON -Depth 100 | Format-Json | Set-Content $PACKAGE_FILE
 
-$deploy_icon_value = [System.Convert]::toInt32("1f680",16)
+$deploy_icon_value = [System.Convert]::toInt32("1f680", 16)
 $deploy_icon = ([System.Char]::ConvertFromUtf32($deploy_icon_value))
 
 Write-Output "-------------------------------------------------------------------"
@@ -44,5 +44,6 @@ git add $PROJECT_FILE $PACKAGE_FILE
 git commit -m "$deploy_icon Deployment of version $VERSION"
 git tag $VERSION
 git push
+git push origin $VERSION
 
 & $PSScriptRoot/export-package.ps1
